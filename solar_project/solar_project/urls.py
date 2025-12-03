6143fe2840d24651fbcf44849fbeb73f658abeb5
A. Mal <jmaldonado250@icloud.com>
2025-12-03 22:39:37 +0000

    Correctly refactored files. This might help with the urls.py and json conflicts for the AI agent.

--- solar_project/solar_project/urls.py
+++ solar_project/solar_project/urls.py
@@ -2,7 +2,7 @@
 URL configuration for solar_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,16 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from solar_monitor.views import SolarSystemViewSet
+# from solar_monitor.views import SolarSystemViewSet
 
 router = DefaultRouter()
-router.register(r'systems', SolarSystemViewSet, basename='solar-system')
+# router.register(r'systems', SolarSystemViewSet, basename='solar-system')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
